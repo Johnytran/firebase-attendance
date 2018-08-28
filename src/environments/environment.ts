78abf77452a0964c95de15bd621eabe13cd93cbc
Johnytran <tuananhasp@gmail.com>
2018-08-28 16:46:03 +1000

    create signup page

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyBYrXhOWWQnUcbZUdP0etTFSK5i8qWYSkg",
+    authDomain: "practice-98cbc.firebaseapp.com",
+    databaseURL: "https://practice-98cbc.firebaseio.com",
+    projectId: "practice-98cbc",
+    storageBucket: "practice-98cbc.appspot.com",
+    messagingSenderId: "324160778994"
+  }
+}
